7ab178ec9d428d171dd2c8a6af872698b100c373
DatNguyen0912 <90879290+DatNguyen0912@users.noreply.github.com>
2022-08-29 19:44:00 +0700

    Add files via upload

---
+++ vd1.rb
@@ -0,0 +1,20 @@
+print "Nhập họ tên của bạn: "
+name1 = gets
+print "Nhập mã sinh viên của bạn: "
+name2 = gets
+print "Xin chào " + name1 + name2
+print "Nhập 1 số từ bàn phím: "
+number = gets
+print "Số vừa nhập: " + number
+puts "Nhập 2 số để so sánh"
+print "Nhập số 1: "
+number1 = gets
+print "Nhập số 2: "
+number2 = gets
+if number1 > number2
+    print "Số lớn hơn là: " + number1
+elsif number1 < number2
+    print "Số lớn hơn là: " + number2
+else
+    print "2 số bằng nhau: " + number1
+end
